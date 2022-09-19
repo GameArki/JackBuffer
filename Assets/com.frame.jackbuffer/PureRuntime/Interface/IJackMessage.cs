a840ab57c2dca85885ee5b849b852653517fc18c
杰克有茶 <chenwansal1@163.com>
2022-09-19 14:46:53 +0800

    ESTABLISHED

---
+++ Assets/com.frame.jackbuffer/PureRuntime/Interface/IJackMessage.cs
@@ -0,0 +1,13 @@
+
+namespace JackBuffer {
+
+    public interface IJackMessage<T> {
+
+        void WriteTo(byte[] dst, ref int offset);
+        void FromBytes(byte[] src, ref int offset);
+        byte[] ToBytes();
+        int GetEvaluatedSize(out bool isCertain);
+
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# JackBuffer
+二进制数据序列化协议, 含自动生成接口
+
+## 依赖
+JackAST: https://github.com/chenwansal/JackAST  
+BufferIO: https://github.com/chenwansal/BufferIO
